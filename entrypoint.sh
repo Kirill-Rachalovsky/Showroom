2829654585c2d5215484b5953030e5844b652637
rachalovsky <devis5308@gmail.com>
2022-08-18 16:06:08 +0300

    nginx & unicorn

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+python manage.py collectstatic --no-input
+
+gunicorn config.wsgi:application --bind 0.0.0.0:8000
